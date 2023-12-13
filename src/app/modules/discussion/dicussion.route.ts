6ed266b3ded76887cb5ba5da4c1e3b1e6d771d32
zarif007 <zarifhuq786@gmail.com>
2023-12-14 05:20:42 +0600

    [Added/Feat]: notification schema

--- src/app/modules/discussion/dicussion.route.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { DiscussionController } from './discussion.controller';
-
-const router = express.Router();
-
-router.get('/', DiscussionController.getFromDB);
-router.post('/', DiscussionController.insertIntoDB);
-
-export const discussionRoute = router;
